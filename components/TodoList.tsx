cd0300f94e6e4c6f3d5748aaad912891903e2648
Isabel Robleda <isabel.robleda@gmail.com>
2024-01-25 16:16:17 +0100

    fixed bug to check

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -1,4 +1,3 @@
-"use client"
 import React, { useEffect, useState } from 'react';
 import Removebtn from './Removebtn';
 import Link from 'next/link';
@@ -10,10 +9,6 @@ interface Todo {
   description: string;
 }
 
-interface TodoRes {
-  todos : Todo[]
-}
-
 const getTodo = async (): Promise<Todo[]> => {
   try {
     const res = await fetch("http://localhost:3000/api/todos", { cache: "no-store" });
@@ -22,8 +17,7 @@ const getTodo = async (): Promise<Todo[]> => {
     }
 
     return res.json();
-  }
-  catch (err) {
+  } catch (err) {
     console.log(err);
     return [];
   }
@@ -35,42 +29,42 @@ const TodoList: React.FunctionComponent = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const fetchedData: TodoRes | undefined = await getTodo();
-  
-        if (fetchedData && Array.isArray(fetchedData.todos)) {
-          console.log('Type of fetchedData.todos:', typeof fetchedData.todos);
-          setTodos(fetchedData.todos);
+        const fetchedData: Todo[] = await getTodo();
+
+        if (Array.isArray(fetchedData)) {
+          console.log('Type of fetchedData:', typeof fetchedData);
+          setTodos(fetchedData);
         }
       } catch (error) {
         console.error(error);
       }
     };
-  
+
     fetchData();
   }, []);
 
   return (
     <div>
-      {todos ? (todos.map((t: Todo) => (
-        <div className='p-4 border border-slate-300 flex justify-between' key={t._id}>
-          <div>
-            <h2 className='font-bold text-2xl'>{t.title}</h2>
-            <p>{t.description}</p>
+      {todos ? (
+        todos.map((t: Todo) => (
+          <div className='p-4 border border-slate-300 flex justify-between' key={t._id}>
+            <div>
+              <h2 className='font-bold text-2xl'>{t.title}</h2>
+              <p>{t.description}</p>
+            </div>
+            <div className='flex gap-2 items-start'>
+              <Removebtn id={t._id} />
+              <Link href={`/editTodo/${t._id}`}>
+                <HiPencilAlt size={24} />
+              </Link>
+            </div>
           </div>
-          <div className='flex gap-2 items-start'>
-            <Removebtn id={t._id} />
-            <Link href={`/editTodo/${t._id}`}>
-              <HiPencilAlt size={24} />
-            </Link>
-          </div>
-        </div>
-      ))) :
-      (
+        ))
+      ) : (
         <div>Loading...</div>
-      )
-      }
+      )}
     </div>
   );
-}
+};
 
 export default TodoList;
