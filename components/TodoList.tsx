29e6f17195d9ed61aa89f4d47b154170d1a9cdf0
Isabel Robleda <isabel.robleda@gmail.com>
2024-01-25 16:37:14 +0100

    fixed bug in get todolist

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -12,29 +12,34 @@ interface Todo {
 const getTodo = async (): Promise<Todo[]> => {
   try {
     const res = await fetch("http://localhost:3000/api/todos", { cache: "no-store" });
+    
     if (!res.ok) {
       throw new Error("Failed to fetch the Todos");
     }
 
-    return res.json();
+    const response = await res.json();
+
+    console.log("Response from server:", res);
+
+    if (!Array.isArray(response.todos)) {
+      throw new Error("Invalid data format from the server");
+    }
+
+    return response.todos;
   } catch (err) {
-    console.log(err);
+    console.error(err);
     return [];
   }
 };
 
 const TodoList: React.FunctionComponent = () => {
-  const [todos, setTodos] = useState<Todo[] | null>(null);
+  const [todos, setTodos] = useState<Todo[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const fetchedData: Todo[] = await getTodo();
-
-        if (Array.isArray(fetchedData)) {
-          console.log('Type of fetchedData:', typeof fetchedData);
-          setTodos(fetchedData);
-        }
+        setTodos(fetchedData);
       } catch (error) {
         console.error(error);
       }
@@ -45,24 +50,20 @@ const TodoList: React.FunctionComponent = () => {
 
   return (
     <div>
-      {todos ? (
-        todos.map((t: Todo) => (
-          <div className='p-4 border border-slate-300 flex justify-between' key={t._id}>
-            <div>
-              <h2 className='font-bold text-2xl'>{t.title}</h2>
-              <p>{t.description}</p>
-            </div>
-            <div className='flex gap-2 items-start'>
-              <Removebtn id={t._id} />
-              <Link href={`/editTodo/${t._id}`}>
-                <HiPencilAlt size={24} />
-              </Link>
-            </div>
+      {todos && todos.map((t: Todo) => (
+        <div className='p-4 border border-slate-300 flex justify-between' key={t._id}>
+          <div>
+            <h2 className='font-bold text-2xl'>{t.title}</h2>
+            <p>{t.description}</p>
           </div>
-        ))
-      ) : (
-        <div>Loading...</div>
-      )}
+          <div className='flex gap-2 items-start'>
+            <Removebtn id={t._id} />
+            <Link href={`/editTodo/${t._id}`}>
+              <HiPencilAlt size={24} />
+            </Link>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
