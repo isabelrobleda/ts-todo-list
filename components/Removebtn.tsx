658965246eac66d7321364c59fecb58209cf21bc
vidishashah25 <vidisha.shah14@gmail.com>
2024-01-29 16:13:57 +0100

    Fixed bug with delete

--- components/Removebtn.tsx
+++ components/Removebtn.tsx
@@ -1,35 +1,34 @@
 "use client"
-
 import React from 'react'
 import { HiOutlineTrash } from 'react-icons/hi';
-import { useRouter } from 'next/navigation';
 
-function Removebtn({id} : {id: any}) {
-  const router = useRouter()
+function Removebtn({ id }: { id: any }) {
+
   const removeTodo = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     e.preventDefault()
     const confirm: any = window.confirm('Are you sure you want to delete this todo?')
-    
-    if(confirm){
-      try{
-        const res = await fetch(`http://localhost:3000/api/todos?id=${id}`, {
-        method: "DELETE"
+
+    if (confirm) {
+      try {
+        const res = await fetch(`http://localhost:3000/api/todos?id=${id}`,
+          {
+            method: "DELETE"
+          }
+        )
+        if (res.ok) {
+          console.log("Deleted Successfully")
+          await window.location.reload();
+        }
       }
-      )
-      if(res.ok){
-        router.refresh()
-      } 
-      router.refresh()
+      catch (err) { console.log(err) }
     }
-    catch(err){ console.log(err)}
-      }
-      
+
   }
   return (
-    <button 
-    className='text-red-400'
-    onClick={(e) => removeTodo(e)}
-    ><HiOutlineTrash size={24}/></button>
+    <button
+      className='text-red-400'
+      onClick={(e) => removeTodo(e)}
+    ><HiOutlineTrash size={24} /></button>
   )
 }
 
